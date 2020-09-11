990dbd2efcccc7f5355fdf6138f203f96c96b942
Carlos Hernandez <CarlosHdz_97@outlook.com>
2020-09-11 10:39:57 -0500

    Metodos que se generaran en el controlador de locations

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class LocationController extends Controller{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(){
+        //
+    }
+
+    public function createCeller(){
+        
+    }
+
+    public function createSection(){
+
+    }
+
+    public function getSections(){
+
+    }
+
+    public function getProductsInLocation(){
+
+    }
+
+    public function setLocations(){
+
+    }
+
+    public function deleteLocations(){
+
+    }
+
+}
