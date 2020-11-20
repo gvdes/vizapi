64bf5bba59c828060a85aa40bac3ef210edb8881
Carlos Hernandez <CarlosHdz_97@outlook.com>
2020-11-20 13:31:06 -0600

    Comentarios

--- app/Http/Controllers/MiniPrinterController.php
+++ app/Http/Controllers/MiniPrinterController.php
@@ -254,9 +254,9 @@ class MiniPrinterController extends Controller{
                     $printer->text(" - UD: ");
                     $printer->setTextSize(2,1);
                     $printer->text($product->pivot->stock."\n");
-                    if($product->pivot->notes){
+                    if($product->pivot->comments){
                         $printer->setTextSize(1,1);
-                        $printer->text($product->pivot->notes."\n");
+                        $printer->text($product->pivot->comments."\n");
                     }
                     $printer->feed(1);
                     $y++;
--- app/Http/Controllers/RequisitionController.php
+++ app/Http/Controllers/RequisitionController.php
@@ -470,6 +470,7 @@ class RequisitionController extends Controller{
         $dominio = explode(':', $who->dominio)[0];
         switch($who->id){
             case 1:
+                /* return ["domain" => "192.168.1.36", "port" => 9100]; */
                 if($for == 4 || $for == 5 || $for == 6 || $for == 9 || $for == 10){
                     return ["domain" => "192.168.1.34", "port" => 9100];
                 }else{
