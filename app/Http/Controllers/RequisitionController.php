4397a023c236763b0d76f29bb46049e21441071a
Carlos Hernandez <CarlosHdz_97@outlook.com>
2021-02-27 14:07:30 -0600

    Se mejoro el pedido automatico

--- app/Http/Controllers/RequisitionController.php
+++ app/Http/Controllers/RequisitionController.php
@@ -541,7 +541,7 @@ class RequisitionController extends Controller{
                 ['min', '>', 0],
                 ['max', '>', 0]
             ]);
-        }, '>', 0)->get();
+        }, '>', 0)->where('_status', 1)->get();
         
         /**OBTENEMOS STOCKS */
         $toSupply = [];
