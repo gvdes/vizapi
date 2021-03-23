9a6e39207520ff7667f75d6aea0676706b1f1407
Carlos Hernandez <CarlosHdz_97@outlook.com>
2021-03-23 11:15:41 -0600

    Se corigio el formato de lapicera x 20

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -1186,7 +1186,7 @@ class PdfController extends Controller{
             $space = '<span style="font-size:1em"><br/></span>';
             if($isInnerPack){
                 $pz = $product['pieces'].' pz';
-                $space = '<span style="font-size:.4em"><br/></span>';
+                /* $space = '<span style="font-size:.4em"><br/></span>'; */
                 $br= .4;
             }
             $tool = '';
@@ -1211,7 +1211,7 @@ class PdfController extends Controller{
                         </div>';
             }
             
-            $this->setImageBackground_area_2(null, $content, $width=37, $height=40, $cols=4, $rows=5, $top_space=-9, $sides_space=10.8, $position=$key+$counter, $top_margin=12.5, $sides_margin=11.6);
+            $this->setImageBackground_area_2(null, $content, $width=37, $height=40, $cols=4, $rows=5, $top_space=-18, $sides_space=11.3, $position=$key+$counter, $top_margin=12.5, $sides_margin=11.6);
         }
     }
     
