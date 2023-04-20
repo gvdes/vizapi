6aaa5298dbffebac9fedfd9d9a4f8662e8373c75
hugomers <hugomo0897@gmail.com>
2023-04-19 18:34:51 -0600

    se cambia el metodo de update prices

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -768,30 +768,6 @@ class LocationController extends Controller{
             })->toArray();
     }
 
-    public function pricesupdates(){
-        return DB::table('products AS P')
-        ->join('product_categories AS PC','PC.id','P._category')
-        ->join('prices_product AS PP','PP._product','P.id')
-        ->whereDate('P.updated_at',Carbon::now()->format('Y-m-d'))
-        ->selectraw('IF(PP.CENTRO = PP.MENUDEO, "OFERTA", "LINEA") AS ESTADO,')
-        ->selectraw('GETSECTION(PC.id) as seccion')
-        ->select('P.code','P.description','PP.CENTRO','PP.ESPECIAL','PP.CAJA','PP.DOCENA','PP.MAYOREO','PP.MENUDEO')
-        ->get()->map(function($row){
-            return [
-                "seccion"=>$row->seccion,
-                "codigo"=>$row->code,
-                "descripcion"=>$row->description,
-                "estado"=>$row->ESTADO,
-                "centro"=>$row->CENTRO,
-                "especial"=>$row->ESPECIAL,
-                "caja"=>$row->CAJA,
-                "docena"=>$row->DOCENA,
-                "mayoreo"=>$row->MAYOREO,
-                "menudeo"=>$row->MENUDEO,
-            ];
-        })->toArray();
-    }
-
     public function updatedprices(){
         return DB::table('products AS P')
             ->join('product_categories AS PC','PC.id','P._category')
