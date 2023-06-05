5ade1cd4d0cbff822a1d05912bf29334c6dcf286
hugomers <hugomo0897@gmail.com>
2023-06-05 14:53:12 -0600

    se cambia la rutade el checador de impresiones de requisisiones por el auth

--- app/Http/Controllers/SalidasController.php
+++ app/Http/Controllers/SalidasController.php
@@ -271,16 +271,17 @@ class SalidasController extends Controller{
         $date = Carbon::now()->format('Y-m-d');
 
         $pedidos = DB::table('requisition')->whereDate('created_at',$date)->where('printed',0)->where('_status',2)->get();
-        if($pedidos){
-            foreach($pedidos as $pedido){
-                $ped[] = $pedido;
-            }
-            $req = implode(", ",$ped);
-            if($ped){
-                $msg = "No se han impreso los pedidos ".$req;
-                $nme = "120363157493041484@g.us";
-                $this->sendWhatsapp($nme,$msg);
-            }
-        }else {return response()->json("no hay brou");}
+        return $pedidos;
+        // if($pedidos){
+        //     foreach($pedidos as $pedido){
+        //         $ped[] = $pedido;
+        //     }
+        //     $req = implode(", ",$ped);
+        //     if($ped){
+        //         $msg = "No se han impreso los pedidos ".$req;
+        //         $nme = "120363157493041484@g.us";
+        //         $this->sendWhatsapp($nme,$msg);
+        //     }
+        // }else {return response()->json("no hay brou");}
     }
 }
